8db64cf27028760aec2ecbdd277c0c609568b8b0
Шамагин Роман Олегович <ROShamagin@sberbank.ru>
2021-09-19 15:45:35 +0300

    c2. Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Avito',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='a19515436',
+    author_email='',
+    description=''
+)
