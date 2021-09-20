901fc605ce38a07513c32d8c33563d2a4f25a950
Roman Shamagin <romaxa55@bk.ru>
2021-09-21 01:05:24 +0300

    Merge remote-tracking branch 'origin/master' Add NEW DEF # Conflicts: #	requirements.txt

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import requests
 from bs4 import BeautifulSoup
 
 CONST_URL = "https://www.avito.ru/sankt-peterburg/noutbuki?f=ASgCAQECAUDwvA0UiNI0AUXGmgwWeyJmcm9tIjo1MDAsInRvIjo1MDAwfQ&user=1"
+DEBUG = True
 
-headers = {
-    "User-Agent":
-        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36",
-}
+
+def get_url(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      " Chrome/41.0.2272.101 Safari/537.36",
+    }
+    s = requests.session()
+    s.headers.update(headers)
+    r = s.get(url, headers=headers)
+    if DEBUG:
+        with open('tmp/debug.html', 'wb') as f:
+            f.write(r.content)
+    return BeautifulSoup(r.content, 'lxml')
+
 
 def get_urls_objects(soup):
     result = {}
@@ -16,18 +28,14 @@ def get_urls_objects(soup):
         result[id] = 'https://www.avito.ru' + tag.find('a').get('href')
     return result
 
+
 def get_one_from_list_objects(soup):
     for id in soup.keys():
         url = soup[id]
-        print(url)
+        soup = get_url(url)
+        break
     # print(soup)
 
 
-s = requests.session()
-s.headers.update(headers)
-r = s.get(CONST_URL, headers=headers)
-# with open('abc.html', 'wb') as f:
-#     f.write(r.content)
-
-soup = BeautifulSoup(r.content, 'lxml')
-get_one_from_list_objects(get_urls_objects(soup))  #list new obgects
+soup = get_url(CONST_URL)
+get_one_from_list_objects(get_urls_objects(soup))  # list new obgects
