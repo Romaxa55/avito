baf431d2e247fb810231553023ebcaa93de99a33
Vyazovoy Denis <indikv@list.ru>
2021-09-25 09:39:08 +0400

    Исправлены некоторые орфографические и пунктуационные ошибки

--- db.py
+++ db.py
@@ -3,24 +3,24 @@ import sqlite3
 class DB:
 
     def __init__(self,db_file):
-        """Init connect Database"""
+        """Подключение к базе"""
         self.connection = sqlite3.connect(db_file)
         self.cursor = self.connection.cursor()
 
     def create_tables(self):
-        """Create tables"""
+        """Создание таблиц"""
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS base
                           (id INTEGER, title TEXT NOT NULL, url TEXT, 
                           price INTEGER, list TEXT, description TEXT, 
                           img TEXT, dt datetime default current_timestamp)""")
 
     def record_exist(self, id):
-        """Проверка существует ли забись в базе"""
+        """Проверка существования записи в базе"""
         result = self.cursor.execute("SELECT COUNT(*) FROM `base` WHERE `id` = ?", (id,))
         return bool(result.fetchone()[0])
 
     def record_add(self, id, data):
-        """Добавляем азпись в базу"""
+        """Добавление записи в базу"""
         try:
             result = self.cursor.execute("INSERT INTO base (id, title, url, price, list, description, img) VALUES (?, ?, ?, ?, ?, ?, ?)",
                                         (id, data['title'], data['url'], data['price'], data['list'],data['description'], data['img']))
@@ -29,10 +29,10 @@ class DB:
             print(e)
 
     def clearOld_record(self):
-        """Чиста базы от старых записей старше n дней"""
+        """Чистка базы от старых записей старше n дней"""
         self.cursor.execute("DELETE FROM base WHERE date(dt) < date('now', '-5 days')")
 
     def close(self):
-        """Close coonnect DB"""
+        """Отключение от базы"""
         self.connection.commit()
         self.connection.close()
